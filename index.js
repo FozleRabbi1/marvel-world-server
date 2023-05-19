26763928fc8af142275ff51c7af69b14c49cd87f
Fozle Rabbi <fozlerabbi9790@gmail.com>
2023-05-19 22:07:37 +0600

    change  port

--- index.js
+++ index.js
@@ -5,15 +5,10 @@ const port = process.env.PORT || 5000;
 const cors = require("cors")
 require('dotenv').config()
 
-
-
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ra0tvnn.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -21,10 +16,8 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
 async function run() {
     try {
-
         const toyCollections = client.db("toyCollections").collection("AllToysData");
 
         app.get("/allToysDatas", async (req, res) => {
@@ -118,5 +111,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ====== ${port}`)
 })
